8e28fb2865f25f4935a5d558b9bfa1bfb64538a0
Adam Hellberg <sharparam@sharparam.com>
2022-04-03 11:15:48 +0200

    Go back to using auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,7 @@ const config = {
     prerender: {
       default: true
     },
-    trailingSlash: 'always'
+    trailingSlash: 'never'
   }
 };
 
